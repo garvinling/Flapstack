ec0bacc7ed31347ac40c2031a7f5becc368e47e5
Garvin <garvin.ling@student.csulb.edu>
2014-02-02 03:19:58 -0800

    Added recent notifications and most recent news feed.  Also: jQuery draggable UI

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,11 @@
 
 $(document).ready(function(){
+
+
+    $( "#data_content_1" ).draggable();
+    $( "#data_content_2").draggable();
+
+
   $.ajaxSetup({ cache: true });
   
   $.getScript('//connect.facebook.net/en_UK/all.js', function(){
@@ -16,18 +22,13 @@ $(document).ready(function(){
  FB.login(function(response) {
    if (response.authResponse) 
    {
-     
      console.log('Welcome!  Fetching your information.... ');
-     
-
      FB.api('/me', function(response) {
      
          console.log('Good to see you, ' + response.name + '.');
-         $("#title").text('Welcome, '+response.name);
+         $("#title").text(response.name);
      
      });
-
-
    } 
    else 
    {
@@ -48,7 +49,6 @@ FB.getLoginStatus(function(response) {
     // request, and the time the access token 
     // and signed request each expire
     window.console&&console.log('Connected');
-
     var uid = response.authResponse.userID;
     var accessToken = response.authResponse.accessToken;
     var date = Math.round(new Date().getTime() / 1000);
@@ -65,8 +65,7 @@ FB.getLoginStatus(function(response) {
 
         if(!response.error && response)
         {
-          console.log(response);
-          $("#location").text(': ' + response.location.name);
+          $("#location").text(response.location.name);
         }
     });
 
@@ -76,57 +75,93 @@ FB.getLoginStatus(function(response) {
         //$("#profile_image").attr('src',response.data.url);
         $("div#profile_image").css("background-image","url("+response.data.url+")");  
 
-        console.log(response.data.url);
+        //console.log(response.data.url);
+    });
 
+    FB.api('me/notifications',{until:'1391212800',limit:'9',include_read:'true'},function(response){
+        //console.log(response.data[0]);
+        sendNotification(response);
+        //$("#header ul").append("<li>"+)
+    });
+
+
+    FB.api('me/home',function(response){
+
+      getPostsFromNewsFeed(response);
+      console.log(response);
 
     });
 
 
-  } else if (response.status === 'not_authorized') {
+
+    FB.api('me/friends',function(response){
+      console.log(response.data.length + "friends exist.");
+      $("#friend_count").text(response.data.length + " connections");
+    });
+
+
+
+  } 
+  else if (response.status === 'not_authorized')
+   {
     // the user is logged in to Facebook, 
     // but has not authenticated your app
     window.console&&console.log('Not Authorized');
 
-  } else {
+  } 
+  else {
     // the user isn't logged in to Facebook.
     window.console&&console.log('Not logged in to facebook');
 
   }
 
- });
+ });  //End FB.getLoginStatus
+});
 
+  function sendNotification(response){
 
-
-/*
-GET graph.facebook.com
-  /{node-id}?
-    fields={first-level}.fields({second-level})
-
-/me?
-    fields=albums.limit(5),posts.limit(5)
-
-*/
-
-
-    function getUserInfoCallback(response)
-    {
-
-        if(response && !response.error)
-        {
-          console.log("SUCCESS");
-
-          console.log(response);
-        }
-
-
+    for (var i=0;i<response.data.length;i++){
+        
+        var from = response.data[i].from.name;
+        //console.log("FOUND:"+response.data[i].title);
+        $("#notify_feed").append("<li id=\"notification"+i+"\"> <code>"+from+"</code>"+response.data[i].title+"</li><br>");
+        $("#notification"+i).hide().fadeIn(1300+(500*i));
     }
+  }//end sendNotification
+
+  function getPostsFromNewsFeed(response){
+
+      for(var i=0; i<response.data.length;i++){
+
+          var post_message = response.data[i].story;
+          var from = "";
+          //console.log(post_title);
+          if(!post_message){
+              //news item is a status update.
+              var post_message = response.data[i].message;
+              var from = response.data[i].from.name;
+              $("#news_feed").append("<li id=\"post"+i+"\"> <code>"+from+"</code>"+post_message+"</li><br>");
+
+          }
+          else
+          {
+         
+          $("#news_feed").append("<li id=\"post"+i+"\">"+post_message+"</li><br>");
 
 
 
+          }
+
+          $("#post"+i).hide().fadeIn(1300+(100*i));
+
+      }
 
 
 
-  });
+  }
+
+
+
 });
 
 
